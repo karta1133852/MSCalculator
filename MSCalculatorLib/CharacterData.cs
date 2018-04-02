cd436dcd0e99aa4b33256476efc78c1b70ef7380
karta1133852 <tr30111@gmail.com>
2018-04-03 04:35:37 +0800

    Create class library project

---
+++ MSCalculatorLib/CharacterData.cs
@@ -0,0 +1,15 @@
+﻿namespace MSCalculatorLib
+{
+  public class CharacterData
+  {
+    public int[] arcLv;
+    public bool[] isArcEquiped;
+    
+
+    public CharacterData()
+    {
+      isArcEquiped = new bool[5] { true, true, true, true, true };
+      arcLv = new int[5] { 11, 12, 9, 8, 1 };
+    }
+  }
+}
---
+++ MSCalculatorLib/GUI/MSData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCalculatorLib.GUI
+{
+  public class MSData
+  {
+    public static int arcMaxLv = 20, arcQty = 5;
+  }
+}
